f14e328372df642c7057ea3ac263167c49ec6ca0
drbugsmn@gmail.com <randy@panache.localdomain>
2018-03-23 23:58:22 -0500

    init

---
+++ composite-service/src/main/kotlin/com/dood/kotlinservices/compositeservice/CompositeServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.dood.kotlinservices.compositeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompositeServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CompositeServiceApplication>(*args)
+}
