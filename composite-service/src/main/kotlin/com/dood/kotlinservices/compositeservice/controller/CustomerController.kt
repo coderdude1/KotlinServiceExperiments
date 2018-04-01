4efda588c3ea9c09b0114b5599c7fb82fcfb9a24
Randy <drbugsmn@gmail.com>
2018-04-01 15:48:16 -0500

    added response body per the book example to set the http response status.  Not sure if it is cleaner than setting a default response via annotation

--- composite-service/src/main/kotlin/com/dood/kotlinservices/compositeservice/controller/CustomerController.kt
+++ composite-service/src/main/kotlin/com/dood/kotlinservices/compositeservice/controller/CustomerController.kt
@@ -1,6 +1,8 @@
 package com.dood.kotlinservices.compositeservice.controller
 
 import com.dood.kotlinservices.compositeservice.model.Customer
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class CustomerController {
     @GetMapping(value = "/customer/{id}")
-    //expression body
-    fun getCustomer(@PathVariable id: Int) = Customer(id, "customer $id")
+    //function body now, directly using ResponseEntity to set httpStatus per book
+    fun getCustomer(@PathVariable id: Int): ResponseEntity<Customer> {
+        return ResponseEntity(Customer(id, "Customer Entity $id"), HttpStatus.OK)
+    }
 }
