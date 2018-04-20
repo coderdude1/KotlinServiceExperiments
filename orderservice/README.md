760993897f1cdb95d931429d80dc979253ecaa37
Randy <drbugsmn@gmail.com>
2018-04-19 21:29:15 -0500

    initial

---
+++ orderservice/README.md
@@ -0,0 +1,8 @@
+#Order Service
+This is a very contrived example so I can experiment with reactive, kotlin, parallel
+calls from the composite to multiple services, and whatever else I can think of
+
+#Things To Research
+1. Kradle (kotlin version of gradle)
+2. Spek/whatever other native kotlin frameworks for testing (vs spock)
+3. security using jwts and webflux
---
+++ orderservice/src/test/kotlin/com/dood/kotlinservices/orderservice/OrderserviceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.dood.kotlinservices.orderservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
