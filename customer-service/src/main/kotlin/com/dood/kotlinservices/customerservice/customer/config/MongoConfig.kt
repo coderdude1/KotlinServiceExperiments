38f0374429f3945e7e75f9a915627f6ea455751a
Randy <drbugsmn@gmail.com>
2018-04-19 21:27:43 -0500

    cleanup

--- customer-service/src/main/kotlin/com/dood/kotlinservices/customerservice/customer/config/MongoConfig.kt
+++ customer-service/src/main/kotlin/com/dood/kotlinservices/customerservice/customer/config/MongoConfig.kt
@@ -1,12 +1,11 @@
 package com.dood.kotlinservices.customerservice.customer.config
 
-import com.dood.kotlinservices.customerservice.customer.repository.CustomerRepository
 import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
 
